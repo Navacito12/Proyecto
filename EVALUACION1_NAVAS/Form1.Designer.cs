4e8dc40f4e87a3832ec691dad566dc3aa02924a6
Navacito12 <20253469@clases.edu.sv>
2025-02-20 23:12:21 -0600

    Add files via upload

---
+++ EVALUACION1_NAVAS/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace EVALUACION1_NAVAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtQueso = new System.Windows.Forms.TextBox();
+            this.txtFrijolQueso = new System.Windows.Forms.TextBox();
+            this.txtRevuelta = new System.Windows.Forms.TextBox();
+            this.txtChicharron = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtHorchata = new System.Windows.Forms.TextBox();
+            this.txtJamaica = new System.Windows.Forms.TextBox();
+            this.btnCalcularTotal = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(209, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PUPUSERIA LA BENCIDIÖN DE DIOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pupusa de Queso ($0.75)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pupusa de Frijo y Queso ($0.50)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pupusa Revuelta ($0.50)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Pupusa Chicahrron ($0.50)";
+            // 
+            // txtQueso
+            // 
+            this.txtQueso.Location = new System.Drawing.Point(283, 84);
+            this.txtQueso.Name = "txtQueso";
+            this.txtQueso.Size = new System.Drawing.Size(100, 20);
+            this.txtQueso.TabIndex = 5;
+            this.txtQueso.Text = "0";
+            // 
+            // txtFrijolQueso
+            // 
+            this.txtFrijolQueso.Location = new System.Drawing.Point(280, 136);
+            this.txtFrijolQueso.Name = "txtFrijolQueso";
+            this.txtFrijolQueso.Size = new System.Drawing.Size(100, 20);
+            this.txtFrijolQueso.TabIndex = 6;
+            this.txtFrijolQueso.Text = "0";
+            // 
+            // txtRevuelta
+            // 
+            this.txtRevuelta.Location = new System.Drawing.Point(283, 195);
+            this.txtRevuelta.Name = "txtRevuelta";
+            this.txtRevuelta.Size = new System.Drawing.Size(100, 20);
+            this.txtRevuelta.TabIndex = 7;
+            this.txtRevuelta.Text = "0";
+            // 
+            // txtChicharron
+            // 
+            this.txtChicharron.Location = new System.Drawing.Point(280, 252);
+            this.txtChicharron.Name = "txtChicharron";
+            this.txtChicharron.Size = new System.Drawing.Size(100, 20);
+            this.txtChicharron.TabIndex = 8;
+            this.txtChicharron.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(496, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Horchata ($0.75)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(496, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Jamaica ($0.75)";
+            // 
+            // txtHorchata
+            // 
+            this.txtHorchata.Location = new System.Drawing.Point(615, 88);
+            this.txtHorchata.Name = "txtHorchata";
+            this.txtHorchata.Size = new System.Drawing.Size(100, 20);
+            this.txtHorchata.TabIndex = 11;
+            this.txtHorchata.Text = "0";
+            // 
+            // txtJamaica
+            // 
+            this.txtJamaica.Location = new System.Drawing.Point(615, 140);
+            this.txtJamaica.Name = "txtJamaica";
+            this.txtJamaica.Size = new System.Drawing.Size(100, 20);
+            this.txtJamaica.TabIndex = 12;
+            this.txtJamaica.Text = "0";
+            // 
+            // btnCalcularTotal
+            // 
+            this.btnCalcularTotal.Location = new System.Drawing.Point(382, 322);
+            this.btnCalcularTotal.Name = "btnCalcularTotal";
+            this.btnCalcularTotal.Size = new System.Drawing.Size(104, 30);
+            this.btnCalcularTotal.TabIndex = 13;
+            this.btnCalcularTotal.Text = "Calcular Total";
+            this.btnCalcularTotal.UseVisualStyleBackColor = true;
+            this.btnCalcularTotal.Click += new System.EventHandler(this.btnCalcularTotal_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(323, 331);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 14;
+            this.lblTotal.Text = "$0.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnCalcularTotal);
+            this.Controls.Add(this.txtJamaica);
+            this.Controls.Add(this.txtHorchata);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtChicharron);
+            this.Controls.Add(this.txtRevuelta);
+            this.Controls.Add(this.txtFrijolQueso);
+            this.Controls.Add(this.txtQueso);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtQueso;
+        private System.Windows.Forms.TextBox txtFrijolQueso;
+        private System.Windows.Forms.TextBox txtRevuelta;
+        private System.Windows.Forms.TextBox txtChicharron;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtHorchata;
+        private System.Windows.Forms.TextBox txtJamaica;
+        private System.Windows.Forms.Button btnCalcularTotal;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ EVALUACION1_NAVAS/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EVALUACION1_NAVAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularTotal_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+
+            // Calcular el costo de las pupusas
+            total += CalcularCantidad(txtQueso.Text, 0.75);
+            total += CalcularCantidad(txtFrijolQueso.Text, 0.50);
+            total += CalcularCantidad(txtRevuelta.Text, 0.50);
+            total += CalcularCantidad(txtChicharron.Text, 0.50);
+
+            // Calcular el costo de las bebidas
+            total += CalcularCantidad(txtHorchata.Text, 0.75);
+            total += CalcularCantidad(txtJamaica.Text, 0.75);
+
+            // Mostrar el total
+            lblTotal.Text = $"${total.ToString("0.00")}";
+        }
+
+        // Método auxiliar para calcular el costo de un ítem
+        private double CalcularCantidad(string cantidadTexto, double precio)
+        {
+            int cantidad;
+            if (int.TryParse(cantidadTexto, out cantidad)) // Intenta convertir el texto a un número
+            {
+                return cantidad * precio; // Si es exitoso, devuelve la cantidad multiplicada por el precio
+            }
+            return 0; // Si no es un número válido, devuelve 0
+        }
+        }
+    }
+
